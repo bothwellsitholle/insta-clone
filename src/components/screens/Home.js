475617d52acf3afcfd3f131b31e0dc37120140e0
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-19 14:09:04 +0200

    home page compelete

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,8 +2,44 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home screen</h1>
+    <div className="home">
+     <div className="card home-card">
+        <h6 style={{marginLeft: 15, marginTop: 15, display: 'block'}}>Bothwell Sithole</h6>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1600137444380-ce5aea5c43c8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGwlMjBwYXBlcnxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" >favorite_border</i>
+            <h6>title</h6>
+            <p>This is an amazing post</p>
+            <input type="text" placeholder="add a comment"/>
+        </div>
+     </div>
+     <div className="card home-card">
+        <h6 style={{marginLeft: 15, marginTop: 15, display: 'block'}}>Bothwell Sithole</h6>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1600137444380-ce5aea5c43c8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGwlMjBwYXBlcnxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" >favorite_border</i>
+
+            <h6>title</h6>
+            <p>This is an amazing post</p>
+            <input type="text" placeholder="add a comment"/>
+        </div>
+     </div>
+     <div className="card home-card">
+        <h6 style={{marginLeft: 15, marginTop: 15, display: 'block'}}>Bothwell Sithole</h6>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1600137444380-ce5aea5c43c8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGwlMjBwYXBlcnxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" >favorite_border</i>
+            <h6>title</h6>
+            <p>This is an amazing post</p>
+            <input type="text" placeholder="add a comment"/>
+        </div>
+     </div>
     </div>
   )
 }
