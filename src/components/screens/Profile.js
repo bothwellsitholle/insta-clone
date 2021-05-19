e3c38b578aa01b875bd9cb57834581d3de0207a3
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-19 13:36:36 +0200

    Profile page complete

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile Screen</h1>
+    <div style={{maxWidth: '700px', margin: '0px auto'}}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          margin: '18px 0px',
+          padding: 10,
+          borderBottom: '1px solid #ccc',
+        }}
+      >
+        <div>
+          <img
+            style={{ height: 160, width: 160, borderRadius: 80 }}
+            src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          />
+        </div>
+        <div>
+          <h4>Bothwell Sithole</h4>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              width: '110%',
+            }}
+          >
+            <h6>12 posts</h6>
+            <h6>12 followers</h6>
+            <h6>12 following</h6>
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+        <img
+          className="item"
+          src='https://deskby.com/image/customer/deskby-andri1615387440.jpg'
+          alt='img'
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
