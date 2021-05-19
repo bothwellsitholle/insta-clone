a9c91268116b3ffa039a9fb1b66e847ddcc29fab
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-19 12:44:11 +0200

    created files for client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup';
+import Login from './components/screens/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
   );
 }
 
